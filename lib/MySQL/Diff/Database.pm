08e324668f6477fd2c57065beff72f8bbd80b806
Adam Spiers <mysqldiff@adamspiers.org>
2011-10-06 12:25:05 +0100

    Bump to version 0.42

--- lib/MySQL/Diff/Database.pm
+++ lib/MySQL/Diff/Database.pm
@@ -26,7 +26,7 @@ Parses a database definition into component parts.
 use warnings;
 use strict;
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 # ------------------------------------------------------------------------------
 # Libraries
--- lib/MySQL/Diff/Utils.pm
+++ lib/MySQL/Diff/Utils.pm
@@ -17,7 +17,7 @@ Currently contains the debug message handling routines.
 use warnings;
 use strict;
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 # ------------------------------------------------------------------------------
 # Libraries
