b1c31855a553590b3c885854329ff30dc7dfee18
Shawn C Carroll <shawn.c.carroll@gmail.com>
2015-10-23 09:04:16 -0500

    Support Older Create table spec
    The Create table spec for MySql 5.0 allowed for the index type to come either before or after the index column names
    https://dev.mysql.com/doc/refman/5.0/en/create-table.html

--- lib/MySQL/Diff/Table.pm
+++ lib/MySQL/Diff/Table.pm
@@ -181,7 +181,7 @@ sub _parse {
             next;
         }
 
-        if (/^(KEY|UNIQUE(?: KEY)?)\s+(\S+?)(?:\s+USING\s+(?:BTREE|HASH|RTREE))?\s*\((.*)\)$/) {
+        if (/^(KEY|UNIQUE(?: KEY)?)\s+(\S+?)(?:\s+USING\s+(?:BTREE|HASH|RTREE))?\s*\((.*)\)(?:\s+USING\s+(?:BTREE|HASH|RTREE))?$/) {
             my ($type, $key, $val) = ($1, $2, $3);
             croak "index '$key' duplicated in table '$name'\n"
                 if $self->{indices}{$key};
