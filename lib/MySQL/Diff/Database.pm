eb99de2484e77a73ee5dc5cda59a78639c984a96
estrabd <estrabd@gmail.com>
2016-06-21 09:03:58 -0500

    Finalizing release.
    Issue-20: Wrapping up and rolling up final release; includes QA
    and last minute changes.

--- lib/MySQL/Diff/Database.pm
+++ lib/MySQL/Diff/Database.pm
@@ -26,7 +26,7 @@ Parses a database definition into component parts.
 use warnings;
 use strict;
 
-our $VERSION = '0.44';
+our $VERSION = '0.46';
 
 # ------------------------------------------------------------------------------
 # Libraries
