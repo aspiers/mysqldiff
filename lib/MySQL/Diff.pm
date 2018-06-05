479a71ebcc987828cb069b380e775897b7025bea
Brett Estrade <brett@cpanel.net>
2018-06-05 13:18:09 -0500

    rolling release 0.60; incrementing the ten's place moving forward to lower version precision...

--- lib/MySQL/Diff.pm
+++ lib/MySQL/Diff.pm
@@ -23,7 +23,7 @@ the second.
 use warnings;
 use strict;
 
-our $VERSION = '0.50';
+our $VERSION = '0.60';
 
 # ------------------------------------------------------------------------------
 # Libraries
--- lib/MySQL/Diff/Utils.pm
+++ lib/MySQL/Diff/Utils.pm
@@ -17,7 +17,7 @@ Currently contains the debug message handling routines.
 use warnings;
 use strict;
 
-our $VERSION = '0.50';
+our $VERSION = '0.60';
 
 # ------------------------------------------------------------------------------
 # Libraries
